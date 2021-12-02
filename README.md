01d87f34de9a5a3036d1265d62016b7a17145e6e
Fede Calendino <fede@calendino.com>
2021-12-02 09:36:59 +0100

    Added: 2021.02

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 
 ## [2021](https://adventofcode.com/2021)
 
-* [Day 1: Sonar Sweep](https://github.com/fedecalendino/adventofcode/tree/main/_2021/_01)
+* [Day 01: Sonar Sweep](https://github.com/fedecalendino/adventofcode/tree/main/_2021/_01)
+* [Day 02: Dive!](https://github.com/fedecalendino/adventofcode/tree/main/_2021/_02)
---
+++ _2021/_02/code.py
@@ -0,0 +1,51 @@
+import utils
+
+
+def part_01(commands: list) -> int:
+    depth, horizontal = 0, 0
+
+    for command, value in commands:
+        match command:
+            case "forward":
+                horizontal += value
+            case "down":
+                depth += value
+            case "up":
+                depth -= value
+            case unknown:
+                raise ValueError(f"{unknown} is a valid command")
+
+    return horizontal * depth
+
+
+def part_02(commands: list) -> int:
+    aim, depth, horizontal = 0, 0, 0
+
+    for command, value in commands:
+        match command:
+            case "forward":
+                horizontal += value
+                depth += aim * value
+            case "down":
+                aim += value
+            case "up":
+                aim -= value
+            case unknown:
+                raise ValueError(f"{unknown} is a valid command")
+
+    return horizontal * depth
+
+
+def formatter(command: str):
+    direction, value = command.split()
+    return direction, int(value)
+
+
+test_values = utils.read("test.txt", formatter)
+input_values = utils.read("input.txt", formatter)
+
+assert part_01(test_values) == 150
+print("Part 01:", part_01(input_values))
+
+assert part_02(test_values) == 900
+print("Part 02:", part_02(input_values))
